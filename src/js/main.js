42e9ff6e6c847a2f63fa204ddb557638e973cf3a
tariq7299 <tr.sar77an78@gmail.com>
2024-07-03 00:40:01 +0300

    - finished demo section

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const swiper = new Swiper(".swiper", {
-        effect: "cards",
-        grabCursor: true,
-         // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-      });
-      
-   
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    effect: "cards",
+    grabCursor: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
+});
